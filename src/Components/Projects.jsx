1859871b6431daa1ff09e97f5937a746dff10439
Orlando <jose.correia.pt.2100@gmail.com>
2020-07-06 00:34:40 +0100

    added new projetc. Updated column presentation

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -6,15 +6,22 @@ class Projects extends Component {
         return (
             <div>
                 <div className="row">
-                    <div className="col-md-6 col-sm-12">
+                    <div className="col-md-12 col-sm-12">
                         <Project
-                            img="/images/profile-page.png"
-                            projectName="My Profile Page"
-                            description="Source code for this profile page your are viewing now. 
-                            This has been created with React as personal learning project. 
-                            It is still in development and the code is not fully optimized yet."
+                            img="/images/covid19-hooks.png"
+                            projectName="Covid-19 Tracker"
+                            description="Another Covid 19 Tracker App. This Application shows information obtained from the API:
+                            'https://covid19.mathdro.id/api'. It uses Hooks to keep and update the state. Personal learning project."
                             url="https://github.com/joseorlandocorreia/profile-page"
                             techs="JavaScript, React, Git"
+                        />
+                        <Project
+                            img="/images/covid19.png"
+                            projectName="Covid-19 Info"
+                            description="Personal project created to learn more advanced concepts of React JS. It show information about Covid19 Worldwide.
+                            Uses Leaflet JS to show a world map with markers and Recharts to show a graphic about the evolution of cases.Work in progress."
+                            url="https://covid-19-oc.netlify.app"
+                            techs="JavaScript, React JS, Leaflet, Recharts"
                         />
                         <Project
                             img="/images/dronnie.png"
@@ -29,37 +36,28 @@ class Projects extends Component {
                             img="/images/map-editor.png"
                             projectName="Map Editor"
                             description="Pixel editor controlled exclusively using keyboard, paint cells in a grid canvas.
-                            Created all the logic from scratch. It uses a Map to store cell states. Implemented Save State to a text file."
+                            Created all the logic from scratch. It uses a Map to store cell states. Implemented Save State to a text file. asfhfhdjafhdjfhjfhdjh jahf"
                             url="https://github.com/joseorlandocorreia/map-editor"
                             techs="Java, Ant, Git, Simple Graphics Library, Git"
                         />
                     </div>
-                    <div className="col-md-6 col-sm-12">
-                        <Project
-                            img="/images/covid19.png"
-                            projectName="Covid19 Info"
-                            description="Personal project created to learn more advanced concepts of React JS. It show information about Covid19 Worldwide.
-                            Uses Leaflet JS to show a world map with markers and Recharts to show a graphic about the evolution of cases. Work in progress"
-                            url="https://covid-19-oc.netlify.app"
-                            techs="JavaScript, React JS, Leaflet, Recharts"
-                        />
-                        <Project
-                            img="/images/supernoid.png"
-                            projectName="Supernoid"
-                            description="Video game created in the fourth week of bootcamp after just two weeks of Java and OOP traning.
+
+                    <Project
+                        img="/images/supernoid.png"
+                        projectName="Supernoid"
+                        description="Video game created in the fourth week of bootcamp after just two weeks of Java and OOP traning.
                             Coded the logic for collision detection, and Factory methods to create all the objects used in the game"
-                            url="https://github.com/Drete457/SUPERNOID"
-                            techs="Java, Ant, Git, SimpleGraphics Library, Gimp"
-                        />
-                        <Project
-                            img="/images/chat-server.png"
-                            projectName="Concurrent Chat Server"
-                            description="Chat Server capable of managing several connections concurrently using threads. Created the logic for Server
+                        url="https://github.com/Drete457/SUPERNOID"
+                        techs="Java, Ant, Git, SimpleGraphics Library, Gimp"
+                    />
+                    <Project
+                        img="/images/chat-server.png"
+                        projectName="Concurrent Chat Server"
+                        description="Chat Server capable of managing several connections concurrently using threads. Created the logic for Server
                             and Client. Implemented client commands like '/quit', more commands can be easly added."
-                            url="https://github.com/joseorlandocorreia/concurrent-chat-server.git"
-                            techs="Java, Ant, Git, Multi-Threading, TCP & Scokets"
-                        />
-                    </div>
+                        url="https://github.com/joseorlandocorreia/concurrent-chat-server.git"
+                        techs="Java, Ant, Git, Multi-Threading, TCP & Scokets"
+                    />
                 </div>
             </div>
         );
