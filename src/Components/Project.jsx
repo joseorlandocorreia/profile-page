467e2585433719a3c59a366e8bc8515a2761e950
Orlando <jose.correia.pt.2100@gmail.com>
2020-07-06 21:08:24 +0100

    Project items now are container class, to give a better representation. Fixed error in projects text. Fixed the size of experience and educations boxes

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 
+const imgStyle = {
+    float: "left",
+    width: "150px",
+    height: "150px",
+    marginRight: "24px",
+};
+
 function Project(props) {
     return (
         <div className="row">
-            <div className="project-box">
+            <div className="container">
                 <div className="col">
-                    <img src={props.img} alt="" />
+                    <img src={props.img} alt="" style={imgStyle} />
                 </div>
                 <div className="col">
                     <h1>{props.projectName}</h1>
                     <p>{props.description}</p>
                     <h6>Technologies: {props.techs}</h6>
                     <br />
-                    <div style={{ float: "right"}}>
+                    <div style={{ float: "right" }}>
                         <a
                             target="_blank"
                             rel="noopener noreferrer"
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -4,44 +4,41 @@ import Project from "./Project";
 class Projects extends Component {
     render() {
         return (
-            <div>
-                <div className="row">
-                    <div className="col-md-12 col-sm-12">
-                        <Project
-                            img="/images/covid19-hooks.png"
-                            projectName="Covid-19 Tracker"
-                            description="Another Covid 19 Tracker App. This Application shows information obtained from the API:
+            <div className="row">
+                <div className="col-md-12 col-sm-12">
+                    <Project
+                        img="/images/covid19-hooks.png"
+                        projectName="Covid-19 Tracker"
+                        description="Another Covid 19 Tracker App. This Application shows information obtained from the API:
                             'https://covid19.mathdro.id/api'. It uses Hooks to keep and update the state. Personal learning project."
-                            url="https://github.com/joseorlandocorreia/profile-page"
-                            techs="JavaScript, React, Git"
-                        />
-                        <Project
-                            img="/images/covid19.png"
-                            projectName="Covid-19 Info"
-                            description="Personal project created to learn more advanced concepts of React JS. It show information about Covid19 Worldwide.
-                            Uses Leaflet JS to show a world map with markers and Recharts to show a graphic about the evolution of cases.Work in progress."
-                            url="https://covid-19-oc.netlify.app"
-                            techs="JavaScript, React JS, Leaflet, Recharts"
-                        />
-                        <Project
-                            img="/images/dronnie.png"
-                            projectName="Dronnie"
-                            description="Video game created in a 48 Hackathon, 
+                        url="https://github.com/joseorlandocorreia/profile-page"
+                        techs="JavaScript, React, Git"
+                    />
+                    <Project
+                        img="/images/covid19.png"
+                        projectName="Covid-19 Info"
+                        description="Personal project created to learn more advanced concepts of React JS. It show information about Covid19 Worldwide.
+                            Uses Leaflet JS to show a world map with markers and Recharts to show a graphic about the evolution of cases. Work in progress."
+                        url="https://covid-19-oc.netlify.app"
+                        techs="JavaScript, React JS, Leaflet, Recharts"
+                    />
+                    <Project
+                        img="/images/dronnie.png"
+                        projectName="Dronnie"
+                        description="Video game created in a 48 Hackathon, 
                             where the player controls a drone and have to fight other drones and viruses spores.
                             Coded skeleton for the game engine. Refactored to be able to add new levels more easily."
-                            url="https://rokingana.github.io/dronnie/"
-                            techs="Java, Gradle, LibGDX, GIMP, SCRUM, Git"
-                        />
-                        <Project
-                            img="/images/map-editor.png"
-                            projectName="Map Editor"
-                            description="Pixel editor controlled exclusively using keyboard, paint cells in a grid canvas.
-                            Created all the logic from scratch. It uses a Map to store cell states. Implemented Save State to a text file. asfhfhdjafhdjfhjfhdjh jahf"
-                            url="https://github.com/joseorlandocorreia/map-editor"
-                            techs="Java, Ant, Git, Simple Graphics Library, Git"
-                        />
-                    </div>
-
+                        url="https://rokingana.github.io/dronnie/"
+                        techs="Java, Gradle, LibGDX, GIMP, SCRUM, Git"
+                    />
+                    <Project
+                        img="/images/map-editor.png"
+                        projectName="Map Editor"
+                        description="Pixel editor controlled exclusively using keyboard, paint cells in a grid canvas.
+                            Created all the logic from scratch. It uses a Map to store cell states. Implemented Save State to a text file."
+                        url="https://github.com/joseorlandocorreia/map-editor"
+                        techs="Java, Ant, Git, Simple Graphics Library, Git"
+                    />
                     <Project
                         img="/images/supernoid.png"
                         projectName="Supernoid"
